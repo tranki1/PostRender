9b0ebe9bef1f4d5617bf93402c2d8ecd02e71d93
tranki1 <kimhieutran100@gmail.com>
2018-06-19 17:47:56 +0300

    create http request to getdata, render feched data to screen and transform data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,36 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state={
+        posts:[]
+    }
+
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response=>{
+            const posts=response.data.slice(0,10);
+            const updatedPosts = posts.map(post=>{
+                return{
+                    ...post,
+                    author:'Kim'
+                }
+            })
+            this.setState({posts:updatedPosts});
+            console.log(this.state.posts);
+        });
+    }
     render () {
+        const updatedPosts = this.state.posts.map(
+            post=>{
+                return (
+                    <Post key={post.id} title={post.title} author={post.author}/>
+                )
+            }
+        )
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {updatedPosts}
                 </section>
                 <section>
                     <FullPost />
