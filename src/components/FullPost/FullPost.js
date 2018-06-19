846d094ea47b1d6b3311aba9125cd755c3a807b8
tranki1 <kimhieutran100@gmail.com>
2018-06-19 18:56:50 +0300

    Delete post from server function

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -18,6 +18,12 @@ class FullPost extends Component {
       }
     }
   }
+  deletePostHandler = ()=> {
+    axios.delete("https://jsonplaceholder.typicode.com/posts/" + this.state.loadedPost.id)
+      .then(response=>{
+        return console.log(response);
+      })
+  }
   render() {
     let post = <p>Please select a Post!</p>;
     if (this.props.id) {
@@ -30,7 +36,7 @@ class FullPost extends Component {
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
-            <button className="Delete">Delete</button>
+            <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
           </div>
         </div>)
     }
